28bfbea0d4b945ac6167a6d9ef95f4939713e70b
Marshall <xtremebass2k@gmail.com>
2017-12-07 00:02:39 -0800

    Fixed issue related to no JWKS_URL being present

--- config.py
+++ config.py
@@ -8,6 +8,12 @@ class Config(object):
     SECRET_KEY = 'not-so-secret'
     SECURITY_PASSWORD_SALT = 'super-secret'
 
+    API_AUDIENCE = 'https://api.benwa.online'
+    AUTH0_DOMAIN = 'choosegoose.auth0.com'
+    AUTH0_CONSUMER_KEY = ''
+    AUTH0_CONSUMER_SECRET = ''
+    JWKS_URL = 'https://' + AUTH0_DOMAIN + '/.well-known/jwks.json'
+
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/benwaonline'
     DEBUG = True
