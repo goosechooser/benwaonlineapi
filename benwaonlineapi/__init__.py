d1b4d7be97ac28e879ba5f8885be22951e1c9978
Marshall <xtremebass2k@gmail.com>
2017-12-20 02:25:53 -0800

    hm how did this get left out

--- benwaonlineapi/__init__.py
+++ benwaonlineapi/__init__.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask, g, url_for, request, flash, redirect, jsonify
-from config import app_config
+from sqlalchemy import create_engine
 
+from benwaonlineapi.config import app_config
 from benwaonlineapi.database import db
 from benwaonlineapi.api import api, manager
+from benwaonlineapi.models import Tag, User
 
 def create_app(config_name=None):
     """
@@ -15,4 +18,26 @@ def create_app(config_name=None):
     manager.init_app(app)
     app.register_blueprint(api)
 
+    @app.cli.command()
+    def initdb():
+        """Initialize the database."""
+        init_db()
+        init_tags(db.session)
+
     return app
+
+def init_db():
+    engine = create_engine(app.config['DB_BASE_URI'])
+    # 20$ says I run this on production
+    engine.execute('DROP DATABASE benwaonline')
+    engine.execute('CREATE DATABASE benwaonline')
+    engine.execute('USE benwaonline')
+
+    import benwaonlineapi.models
+    db.create_all()
+
+def init_tags(session):
+    tag = Tag(name='benwa')
+    session.add(tag)
+
+    session.commit()
