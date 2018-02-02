6c0af78ca7934200ce7a672e0b27446642ad80e6
Marshall <xtremebass2k@gmail.com>
2018-02-02 04:53:37 -0800

    in prep for some db changes 😩

--- benwaonlineapi/__init__.py
+++ benwaonlineapi/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask, g, url_for, request, flash, redirect, jsonify
 from sqlalchemy import create_engine
 
 from benwaonlineapi.config import app_config
-from benwaonlineapi.database import db
+from benwaonlineapi.database import db, migrate
 from benwaonlineapi.api import api, manager
 from benwaonlineapi import models
 
@@ -29,6 +29,7 @@ def create_app(config_name=None):
     app.config.from_object(app_config[config_name])
 
     db.init_app(app)
+    migrate.init_app(app, db)
     manager.init_app(app)
     app.register_blueprint(api)
 
