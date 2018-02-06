d4e73e323d6c13a11b1e5149720dd23f4251e279
Marshall <xtremebass2k@gmail.com>
2018-02-06 06:36:04 -0800

    so fresh and so clean

--- benwaonlineapi/manager.py
+++ benwaonlineapi/manager.py
@@ -6,8 +6,15 @@ from benwaonlineapi.resources import (
 )
 
 manager = Api(blueprint=Blueprint('api', __name__, url_prefix='/api'))
-
-manager.route(PostList, 'post_list', '/posts', '/tags/<int:id>/posts')
+# THIS IS HOW U WANNA DO IT MY DUDE
+test = {
+    'resource': PostList,
+    'view': 'post_list',
+    'urls': ['/posts', '/tags/<int:id>/posts'],
+    'url_rule_options': {}
+}
+manager.resources = [test]
+# manager.route(PostList, 'post_list', '/posts', '/tags/<int:id>/posts')
 manager.route(PostDetail, 'post_detail', '/posts/<int:id>')
 manager.route(PostRelationship, 'post_tags', '/posts/<int:id>/relationships/tags')
 manager.route(TagList, 'tag_list', '/tags', '/posts/<int:id>/tags')
