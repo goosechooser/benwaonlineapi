d5c0b4a23cbdbc320686e90d71ad23ae1bf47ee9
Marshall <xtremebass2k@gmail.com>
2018-02-14 02:21:35 -0800

    Cleaned up. Added 'memoize_view'

--- benwaonlineapi/endpoint.py
+++ benwaonlineapi/endpoint.py
@@ -1,11 +1,6 @@
 from flask_rest_jsonapi import ResourceDetail, ResourceList, ResourceRelationship
 from marshmallow_jsonapi.flask import Relationship
 from benwaonlineapi import schemas
-from benwaonlineapi.resources import (
-    PostDetail, PostList, PostRelationship,
-    TagDetail, TagList, TagRelationship,
-    UserDetail, UserList, UserRelationship
-)
 
 class EndpointFactory(object):
     def __init__(self):
@@ -34,35 +29,43 @@ class EndpointFactory(object):
     def related_dict(self, resource, related_field, related_field_name):
         # Set up the main resource
         type_ = resource.schema.Meta.type_
+        id_ = '<int:id>'
         view = type_ + '_' + related_field_name
-        suffix = 'list' if related_field.many else 'detail'
-        self.memo[view] = {'resource': resource,
-                           'view': view,
-                           'urls': ['/'.join(['', type_, '<int:id>', 'relationships', related_field_name])],
-                           'url_rule_options': {}
-                           }
+        url = '/'.join(['', type_, id_, 'relationships', related_field_name])
+        self.memoize_view(resource, view, url)
 
         # Add related resource views to other resources
+        suffix = 'list' if related_field.many else 'detail'
         related_view = related_field.type_ + '_' + suffix
-        type_selector = '<string:type_>'
-        related_url = '/'.join(['', type_selector,
-                                '<int:id>', related_field_name])
-        self.memo.get(related_view)['urls'].append(related_url)
+        type_id = '<int:' + type_ + '_id>'
+        related_url = '/'.join(['', type_,
+                                type_id, related_field_name])
+
+        self.memoize_view(resource, related_view, related_url)
+
+            # related['urls'].append(related_url)
+
+    def memoize_view(self, resource, view, url):
+        try:
+            self.memo.get(view)['urls'].append(url)
+        except TypeError:
+            self.memo[view] = {
+                'resource': resource,
+                'view': view,
+                'urls': [url],
+                'url_rule_options': {}
+            }
 
     def list_endpoint(self, resource):
         type_ = resource.schema.Meta.type_
         view = type_ + '_' + 'list'
-        self.memo[view] = {'resource': resource,
-                           'view': view,
-                           'urls': ['/'.join(['', type_])],
-                           'url_rule_options': {}
-                           }
+        url = '/'.join(['', type_])
+
+        self.memoize_view(resource, view, url)
 
     def detail_endpoint(self, resource):
         type_ = resource.schema.Meta.type_
         view = type_ + '_' + 'detail'
-        self.memo[view] = {'resource': resource,
-                           'view': view,
-                           'urls': ['/'.join(['', type_, '<int:id>'])],
-                           'url_rule_options': {}
-                           }
+        url = '/'.join(['', type_, '<int:id>'])
+
+        self.memoize_view(resource, view, url)
