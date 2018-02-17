40c0ff8ecf27d84d5a799bd2ada49ad7c8ffde03
Marshall <xtremebass2k@gmail.com>
2018-02-17 06:23:42 -0800

    Script to convert existing tag names into a different format

--- scripts/remove_duplicate_tags.py
+++ scripts/remove_duplicate_tags.py
@@ -1,13 +1,12 @@
 '''Script to remove Tag entries that have the same name value but differing IDs.'''
 from sqlalchemy import create_engine
-from benwaonlineapi.models import Tag, Post
+from benwaonlineapi.models import Tag, Post, posts_tags
 from benwaonlineapi.database import db
 
 from run import app
 
 def connect():
     engine = create_engine('mysql+pymysql://root:root@localhost:3306/')
-    # 20$ says I run this on production
     engine.execute('USE test_db')
 
     connection = db.engine.connect()
@@ -16,15 +15,14 @@ def connect():
     options = dict(bind=connection, binds={})
     session = db.create_scoped_session(options=options)
     return session
-    # import benwaonlineapi.models
-    # db.create_all()
 
 def find_duplicates(session):
     memo = {}
     tags = Tag.query.all()
-    print('old_tags', [tag.id for tag in tags])
+    # print('old_tags', [tag.id for tag in tags])
 
     for tag in tags:
+        print('Tag name is', tag.name)
         memo[tag.name] = memo.get(tag.name, [])
         memo[tag.name].append(tag)
 
@@ -44,24 +42,24 @@ def remove_duplicates(session, duplicates):
     posts = Post.query.all()
     for d in duplicates:
         for post in posts:
-            tags = post.tags.order_by(Tag.created_on).all()
+            tags = post.tags
+            tags.sort(key=lambda tag: tag.created_on)
             if not set(d['replace']).isdisjoint(tags):
                 print('old tags', [(tag.name, tag.id) for tag in tags])
                 post.tags = [d['oldest'] if tag in d['replace'] else tag for tag in tags]
 
-                tags = post.tags.order_by(Tag.created_on).all()
-                print('new tags', [(tag.name, tag.id) for tag in tags])
+                # tags = post.tags.order_by(Tag.created_on).all()
+                print('new tags', [(tag.name, tag.id) for tag in post.tags])
 
                 for r in d['replace']:
-                    Tag.query.filter(Tag.id == r.id).delete(synchronize_session=False)
-                session.commit()
+                    print(r.name, r.id)
+                    session.delete(Tag.query.get(r.id))
+                    # Tag.query.filter(posts_tags.c.tags_id == r.id).delete(synchronize_session=False)
 
-def setup():
+if __name__ == '__main__':
     with app.app_context():
         connect()
         duplicates = find_duplicates(db.session)
         remove_duplicates(db.session, duplicates)
-
-if __name__ == '__main__':
-        setup()
+        db.session.commit()
 
