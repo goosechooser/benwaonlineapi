9725e17d48a77381d4bf763545cc70083b2b5e06
Marshall <xtremebass2k@gmail.com>
2018-03-01 06:09:49 -0800

    refactored to make use of new fixtures/test class

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,87 +1,74 @@
-from datetime import datetime, timedelta
 import json
+from datetime import datetime, timedelta
 
 import pytest
-from jose import jwt, exceptions
-from flask_rest_jsonapi.exceptions import JsonApiException
 from flask import url_for
+from jose import exceptions, jwt
 
 from benwaonlineapi import util
-post_headers = {'Accept': 'application/vnd.api+json',
-                'Content-Type': 'application/vnd.api+json'}
+from flask_rest_jsonapi.exceptions import JsonApiException
+from tests.helpers import generate_jwt
 
-def get_pem(fname):
-    with open(fname, 'r') as f:
-        return f.read()
-
-PRIV_KEY = get_pem('keys/benwaonline_api_test_priv.pem')
-PUB_KEY = get_pem('keys/benwaonline_api_test_pub.pem')
 ISSUER = 'issuer'
 API_AUDIENCE = 'audience'
 
-@pytest.fixture(scope='function')
-def jwks():
-    with open('keys/test_jwks.json', 'r') as f:
-        JWKS = json.load(f)
-
-    yield JWKS
-
-def generate_jwt(claims):
+def generate_jwt(claims, priv_key):
     ''' Generates a JWT'''
     headers = {
         'typ': 'JWT',
         'alg': 'RS256',
         'kid': 'benwaonline_api_test'
     }
-    return jwt.encode(claims, PRIV_KEY, algorithm='RS256', headers=headers)
+    return jwt.encode(claims, priv_key, algorithm='RS256', headers=headers)
 
-def test_verify_token_invalid_signature(jwks):
-    now = (datetime.utcnow() - datetime(1970, 1, 1))
-    exp_at = now + timedelta(seconds=300)
+class TestVerifyToken(object):
+    def test_invalid_signature(self, jwks, priv_key):
+        now = (datetime.utcnow() - datetime(1970, 1, 1))
+        exp_at = now + timedelta(seconds=300)
 
-    claims = {
-        'iss': ISSUER,
-        'aud': API_AUDIENCE,
-        'sub': '6969',
-        'iat': now.total_seconds(),
-        'exp': exp_at.total_seconds()
-    }
-    token = generate_jwt(claims)
+        claims = {
+            'iss': ISSUER,
+            'aud': API_AUDIENCE,
+            'sub': '6969',
+            'iat': now.total_seconds(),
+            'exp': exp_at.total_seconds()
+        }
+        token = generate_jwt(claims, priv_key)
 
-    jwks['keys'][0]['kid'] = 'invalid'
-    with pytest.raises(JsonApiException):
-        util.verify_token(token, jwks)
+        jwks['keys'][0]['kid'] = 'invalid'
+        with pytest.raises(JsonApiException):
+            util.verify_token(token, jwks)
 
-def test_verify_token_invalid_audience(jwks):
-    claims = {
-        'iss': ISSUER,
-        'aud': 'invalid'
-    }
-    token = generate_jwt(claims)
+    def test_invalid_audience(self, jwks, priv_key):
+        claims = {
+            'iss': ISSUER,
+            'aud': 'invalid'
+        }
+        token = generate_jwt(claims, priv_key)
 
-    with pytest.raises(JsonApiException):
-        util.verify_token(token, jwks)
+        with pytest.raises(JsonApiException):
+            util.verify_token(token, jwks)
 
-def test_verify_token_invalid_issuer(jwks):
-    claims = {
-        'iss': 'invalid',
-        'aud': API_AUDIENCE
-    }
-    token = generate_jwt(claims)
-    with pytest.raises(JsonApiException):
-        util.verify_token(token, jwks)
+    def test_invalid_issuer(self, jwks, priv_key):
+        claims = {
+            'iss': 'invalid',
+            'aud': API_AUDIENCE
+        }
+        token = generate_jwt(claims, priv_key)
+        with pytest.raises(JsonApiException):
+            util.verify_token(token, jwks)
 
-def test_verify_token_expired(jwks):
-    now = (datetime(1971, 1, 1) - datetime(1970, 1, 1))
-    exp_at = now + timedelta(seconds=300)
+    def test_token_expired(self, jwks, priv_key):
+        now = (datetime(1971, 1, 1) - datetime(1970, 1, 1))
+        exp_at = now + timedelta(seconds=300)
 
-    claims = {
-        'iss': ISSUER,
-        'aud': API_AUDIENCE,
-        'sub': '6969',
-        'iat': now.total_seconds(),
-        'exp': exp_at.total_seconds()
-    }
-    token = generate_jwt(claims)
-    with pytest.raises(JsonApiException):
-        util.verify_token(token, jwks)
+        claims = {
+            'iss': ISSUER,
+            'aud': API_AUDIENCE,
+            'sub': '6969',
+            'iat': now.total_seconds(),
+            'exp': exp_at.total_seconds()
+        }
+        token = generate_jwt(claims, priv_key)
+        with pytest.raises(JsonApiException):
+            util.verify_token(token, jwks)
