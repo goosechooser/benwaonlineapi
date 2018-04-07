2dbac47be8ddb2495f29d323a6e192af4b88306f
Marshall <xtremebass2k@gmail.com>
2018-04-07 03:49:27 -0700

    the magic of logging

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 import os
-
+import logging
 from benwaonlineapi import create_app
 
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
