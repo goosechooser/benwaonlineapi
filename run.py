2e5e6e686e9b8e77d30758c961768cc80a87479c
Marshall <xtremebass2k@gmail.com>
2018-04-11 06:21:54 -0700

    we want logging during dev AND prod

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from benwaonlineapi import create_app
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 
-if __name__ != '__main__':
+if os.getenv('FLASK_CONFIG') == 'prod':
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
